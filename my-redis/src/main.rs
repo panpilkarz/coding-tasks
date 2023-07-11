4b3f23aeb412d478777b12351fdec5d5205764c6
panpilkarz <panpilkarz@protonmail.com>
2023-07-11 09:04:32 +0200

    Add my redis (tokio tutorial)

---
+++ my-redis/src/main.rs
@@ -0,0 +1,63 @@
+use bytes::Bytes;
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    let db = Arc::new(Mutex::new(HashMap::<String, Bytes>::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        
+        let db = db.clone();
+
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db: Db) {
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let mut db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd)
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+
+}    
+
+// async fn process(socket: TcpStream) {
+//     let mut connection = Connection::new(socket);
+
+//     if let Some(frame) = connection.read_frame().await.unwrap() {
+//         println!("> {:?}", frame);
+        
+//         let response = Frame::Error("unimplemented".to_string());
+//         connection.write_frame(&response).await.unwrap();
+//     }
+// }
